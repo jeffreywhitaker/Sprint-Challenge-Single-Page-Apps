1689377e55e9ea98b37a06f198dde7d71187909a
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-08-23 10:25:35 -0700

    added semantic ui tab

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -2,17 +2,44 @@ import React from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
+const panes = [
+    { 
+        menuItem: 'Home', 
+        render: () => <></> 
+    },
+    { 
+        menuItem: 'Characters', 
+        render: () => <></>  
+    },
+    { 
+        menuItem: 'Location', 
+        render: () => <></>  
+    },
+    { 
+        menuItem: 'Episodes', 
+        render: () => <></>  
+    },
+  ]
+
 // TODO: Add missing tabs below
 // Take a look at React Semantic UI tabs 
 // https://react.semantic-ui.com/modules/tab/
 export default function TabNav() {
     return (
-        <>
-            <NavLink to="/" exact>Home</NavLink>
-            <NavLink to="/character">Characters</NavLink>
-            <NavLink to="/location">Locations</NavLink>
-            <NavLink to="/episode">Episodes</NavLink>
-        </>
+        <Menu>
+            <Menu.Item as={NavLink} to="/" exact>
+                Home
+            </Menu.Item>
+            <Menu.Item as={NavLink} to="/character">
+                Character
+            </Menu.Item>
+            <Menu.Item as={NavLink} to="/location">
+                Location
+            </Menu.Item>
+            <Menu.Item as={NavLink} to="/episode">
+                Episode
+            </Menu.Item>
+        </Menu>
     )
 };
 
