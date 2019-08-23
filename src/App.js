c206ba0852b28a2e4ccdb0bf835f1d609b8d2a42
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-08-23 08:40:18 -0700

    setting up routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { Route } from "react-router-dom";
+
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
+
+import CharacterList from "./components/CharacterList";
+import LocationsList from "./components/LocationsList";
+import EpisodeList from "./components/EpisodeList";
 
 
 export default function App() {
@@ -8,6 +14,10 @@ export default function App() {
     <main>
       <Header />
       {/* <TabNav /> */}
+      <CharacterList />
+      <Route path="/character" component={CharacterList} />
+      <Route path="/location" component={LocationsList} />
+      <Route path="/episode" component={EpisodeList} />
     </main>
   );
 }
---
+++ src/components/EpisodeList.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import CharacterCard from "./CharacterCard";
+
+export default function EpisodeList() {
+    return (
+        <div></div>
+    )
+}
